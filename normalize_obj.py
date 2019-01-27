aaa222ddb95e7d1e37352b63fa56ed8639c138de
Lex Song <LexSong.tw@gmail.com>
2019-01-27 13:43:21 +0800

    Add rotation matrix

--- normalize_obj.py
+++ normalize_obj.py
@@ -14,6 +14,9 @@ cos_lon = math.cos(lon)
 
 earth_radius = 6378137
 T = np.array([cos_lat * cos_lon, cos_lat * sin_lon, sin_lat]) * earth_radius
+Rz = np.array([[cos_lon, sin_lon, 0], [-sin_lon, cos_lon, 0], [0, 0, 1]])
+Ry = np.array([[cos_lat, 0, sin_lat], [0, 1, 0], [-sin_lat, 0, cos_lat]])
+R = Ry @ Rz
 
 input_file = Path(sys.argv[3])
 output_file = input_file.with_name(input_file.stem + '.2.obj')
@@ -22,7 +25,7 @@ with input_file.open() as in_, output_file.open('w') as out:
     for line in tqdm(in_):
         if line.startswith("v "):
             vertex = np.fromstring(line[2:], sep=' ')
-            vertex -= T
+            vertex = R @ (vertex - T)
             line = "v {} {} {}\n".format(*vertex)
 
         out.write(line)
