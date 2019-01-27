d3f2e7e2b3dffedfed1ad6a5a38d7eecf2ace77d
Lex Song <LexSong.tw@gmail.com>
2019-01-27 12:55:16 +0800

    Refactor normalize_obj.py

--- normalize_obj.py
+++ normalize_obj.py
@@ -6,21 +6,26 @@ from tqdm import tqdm
 lat = math.radians(float(sys.argv[1]))
 lon = math.radians(float(sys.argv[2]))
 
-input_file = Path(sys.argv[3])
-output_file = input_file.with_name(input_file.stem + '.2.obj')
+sin_lat = math.sin(lat)
+cos_lat = math.cos(lat)
+sin_lon = math.sin(lon)
+cos_lon = math.cos(lon)
 
 earth_radius = 6378137
-x = earth_radius * math.cos(lat) * math.cos(lon)
-y = earth_radius * math.cos(lat) * math.sin(lon)
-z = earth_radius * math.sin(lat)
+tx = earth_radius * cos_lat * cos_lon
+ty = earth_radius * cos_lat * sin_lon
+tz = earth_radius * sin_lat
+
+input_file = Path(sys.argv[3])
+output_file = input_file.with_name(input_file.stem + '.2.obj')
 
 with input_file.open() as in_, output_file.open('w') as out:
     for line in tqdm(in_):
         if line.startswith("v "):
             vertex = [float(x) for x in line.split()[1:]]
-            vertex[0] -= x
-            vertex[1] -= y
-            vertex[2] -= z
+            vertex[0] -= tx
+            vertex[1] -= ty
+            vertex[2] -= tz
 
             line = "v {} {} {}\n".format(*vertex)
 
